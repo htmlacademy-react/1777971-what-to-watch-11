ac7c95faa37be3b66be88a6fd80d07355252d77f
Семен Пурнемцев <purnemtzev.semen@yandex.ru>
2022-10-21 16:01:16 +0300

    add pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,8 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+import { IPromo } from '../../index';
+
+function App({promoFilm}:{promoFilm: IPromo}): JSX.Element {
+  return <MainPage promoFilm={promoFilm} />;
 }
 
 export default App;
