9e3d37ae3751d78a6d2c016411b1acad9d2aed98
Семен Пурнемцев <purnemtzev.semen@yandex.ru>
2022-10-21 13:54:11 +0300

    add pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,12 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/MainPage';
+
+type Props = {
+    title: string; //The Grand Budapest Hotel
+    genre: string; //Drama
+    releaseDate: number; //2014
+}
+function App({title, genre, releaseDate}: Props): JSX.Element {
+  return <MainPage title={title} genre={genre} releaseDate={releaseDate} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -5,9 +5,12 @@ import App from './components/app/app';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+const title = 'The Grand Budapest Hotel';
+const genre = 'Drama';
+const releaseDate = 2014;
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App title={title} genre={genre} releaseDate={releaseDate} />
   </React.StrictMode>,
 );
