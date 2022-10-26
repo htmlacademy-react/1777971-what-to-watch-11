042f2886e10dc025c73b413af3cb125cb73d5c7f
Семен Пурнемцев <purnemtzev.semen@yandex.ru>
2022-10-26 17:01:27 +0300

    add const from path

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -8,24 +8,26 @@ import PlayerPage from '../../pages/player-page/player-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import MoviePageDetails from '../../pages/movie-page-details/movie-page-details';
 import PrivateRoute from '../private-route/private-route';
+import { AppRoute } from '../../const';
+
 function App({promoFilm}:{promoFilm: IPromo}): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'>
+        <Route path={AppRoute.Root}>
           <Route index element={<MainPage promoFilm={promoFilm}/>}/>
-          <Route path='login' element={<SignInPage/>}/>
-          <Route path='mylist' element={
+          <Route path={AppRoute.Login} element={<SignInPage/>}/>
+          <Route path={AppRoute.MyList} element={
             <PrivateRoute hasAccess={false}>
               <MyList />
             </PrivateRoute>
           }
           />
 
-          <Route path='player/:id' element={<PlayerPage/>}/>
-          <Route path='films/:id/'>
+          <Route path={AppRoute.PlayerId} element={<PlayerPage/>}/>
+          <Route path={AppRoute.FilmsId}>
             <Route index element={<MoviePageDetails/>}/>
-            <Route path='review' element={<AddReviewPage/>}/>
+            <Route path={AppRoute.Review} element={<AddReviewPage/>}/>
           </Route>
         </Route>
         <Route path='*' element={<NotFoundPage/>}/>
